adf14a02ee3da4812237b3b3b87c1846d27798fc
davedwight <dpdwight@gmail.com>
2021-07-08 21:38:13 -0600

    adds .env file with dynamic port

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/')));
 
 // global middlewares and the user's router need to be connected here
 const usersRouter = require('./users/users-router');
@@ -19,6 +20,13 @@ server.use('/api/users', usersRouter);
 
 server.get('/', logger, (req, res, next) => { // eslint-disable-line
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.get('/api', (req, res) => {
+  console.log(req);
+  res.json({
+      message: "successfully deployed",
+  })
 });
 
 server.use('*', notFound);
